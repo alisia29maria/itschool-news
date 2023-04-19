8a0f944bcb456ad672f48552152c11d60875484c
alisia29maria <alisia29maria@gmail.com>
2023-04-19 22:27:28 +0300

    initialize project

---
+++ README.md
@@ -0,0 +1,5 @@
+This is my first project using React.
+The project displays news from various categories, such as Tech, Football and Law.
+You also have the possibility to add/remove your favorite news.
+
+The project is deployed here: https://itschool-news-third-project.netlify.app
---
+++ src/api/endpoints.js
@@ -0,0 +1,17 @@
+const API_KEY = "4dc4e544-6a96-4b76-8d74-8ca78840671c";
+
+export function getNewsCategoriesEndpoint(
+  category,
+  pageNumber = 1,
+  pageSize = 20
+) {
+  const queryParams = `?api-key=${API_KEY}&section=${category}&show-fields=all&page-size=${pageSize}&page=${pageNumber}`;
+
+  return `https://content.guardianapis.com/search${queryParams}`;
+}
+
+export function getNewsDetailsEndpoint(newsId) {
+  const queryParams = `?api-key=${API_KEY}&show-fields=all`;
+
+  return `https://content.guardianapis.com/${newsId}${queryParams}`;
+}
